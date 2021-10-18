7f0b181a183da55e1a6c123eda21672b10a63e7e
Ovidiu Gherman <ovidiug@eed.usv.ro>
2021-10-18 17:26:20 +0300

    xcvxdvczxvccxvxc

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("No content 3, cloned!");
+            Console.WriteLine("No content 3, cloned, crappy app!");
         }
     }
 }
